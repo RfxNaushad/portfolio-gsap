ba1a1a2709d5b38de4d41f368fba45d6d9f3429b
RfxNaushad <web.naushad@gmail.com>
2024-10-22 15:11:18 +0600

    feat: first 2 section

--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -7,7 +7,11 @@ import RecentProjects from "../components/RecentProjects";
 import Testimonial from "../components/Testimonial";
 import WhatIDo from "../components/WhatIDo";
 import WorkExperience from "../components/WorkExperience";
-import TextEffects from "../components/TextEffects";
+import TextMarquee from "../components/TextMarquee";
+
+
+
+
 
 
 function MainPage() {
@@ -85,6 +89,7 @@ function MainPage() {
             </nav>
 
             <Hero />
+            <TextMarquee />
             <About />
             {/* <WhatWeDo /> */}
             <WhatIDo />
@@ -93,6 +98,7 @@ function MainPage() {
             <Testimonial />
             <ProjectShowcase />
             <Footer />
+
         </div>
     );
 }
